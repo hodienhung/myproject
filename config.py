643465350125008764ebe3243004e028ece5007b
Hung232006 <24107264@st.phenikaa-uni.edu.vn>
2025-12-14 20:22:13 +0700

    edd

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     VNP_TMN_CODE = "NJFA55LY"   # Mã định danh merchant
     VNP_HASH_SECRET = "BN6AYXN4DSTT4ENVLFGUDHD96XV34UIM"  # Chuỗi bí mật tạo checksum
     VNP_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"  # URL sandbox
-    VNPAY_RETURN_URL = "http://127.0.0.1:5000/vnpay_return"
+    VNPAY_RETURN_URL = "https://hienpuremom.onrender.com/vnpay_return"
   # URL nhận kết quả trả về
