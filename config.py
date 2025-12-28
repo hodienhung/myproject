6d823843732115817561f4bd52fff6a7084b83b9
Hung232006 <24107264@st.phenikaa-uni.edu.vn>
2025-12-28 22:53:07 +0700

    gg

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 import os
+from dotenv import load_dotenv
 
+# Chỉ cần ở local dev để đọc file .env
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kidcare_user:RM6eDLHspfhCNWrl8n4IwGeeLMSICD4U@dpg-d4t4h5chg0os73clbqb0-a.oregon-postgres.render.com:5432/kidcare"
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # VNPay Config
-    VNP_TMN_CODE = "NJFA55LY"   # Mã định danh merchant
-    VNP_HASH_SECRET = "BN6AYXN4DSTT4ENVLFGUDHD96XV34UIM"  # Chuỗi bí mật tạo checksum
-    VNP_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"  # URL sandbox
-    VNPAY_RETURN_URL = "http://127.0.0.1:5000/vnpay_return"
-  # URL nhận kết quả trả về
+    VNP_TMN_CODE = os.getenv("VNP_TMN_CODE")
+    VNP_HASH_SECRET = os.getenv("VNP_HASH_SECRET")
+    VNP_URL = os.getenv("VNP_URL", "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html")
+    VNPAY_RETURN_URL = os.getenv("VNPAY_RETURN_URL", "http://127.0.0.1:5000/vnpay_return")
+
+    # Google OAuth
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
